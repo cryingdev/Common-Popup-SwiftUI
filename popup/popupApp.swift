85899f82edb85d163010bd1cb4d974249e9198aa
cryingdev <33308581+cryingdev@users.noreply.github.com>
2024-08-06 19:22:45 +0900

    Initial Commit

---
+++ popup/popupApp.swift
@@ -0,0 +1,17 @@
+//
+//  popupApp.swift
+//  popup
+//
+//  Created by G.K.LEE on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct popupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
